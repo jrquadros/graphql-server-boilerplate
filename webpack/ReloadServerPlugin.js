218157a62942a315f17ff3297fc204016cf5231f
jrquadros <jrmquadros@gmail.com>
2021-07-24 00:27:52 -0300

    feat(app): pre commit hook

--- webpack/ReloadServerPlugin.js
+++ webpack/ReloadServerPlugin.js
@@ -1,52 +1,53 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // https://github.com/ericclemmons/reload-server-webpack-plugin/blob/master/src/ReloadServerPlugin.js
 
 const cluster = require('cluster')
 const path = require('path')
 
 const defaultOptions = {
-  script: 'server.js',
+	script: 'server.js',
 }
 
 class ReloadServerPlugin {
-  constructor({ script } = defaultOptions) {
-    this.done = null
-    this.workers = []
+	constructor({ script } = defaultOptions) {
+		this.done = null
+		this.workers = []
 
-    cluster.setupMaster({
-      exec: path.resolve(process.cwd(), script),
-    })
+		cluster.setupMaster({
+			exec: path.resolve(process.cwd(), script),
+		})
 
-    cluster.on('online', (worker) => {
-      this.workers.push(worker)
+		cluster.on('online', (worker) => {
+			this.workers.push(worker)
 
-      if (this.done) {
-        this.done()
-      }
-    })
-  }
+			if (this.done) {
+				this.done()
+			}
+		})
+	}
 
-  apply(compiler) {
-    compiler.hooks.afterEmit.tap(
-      {
-        name: 'reload-server',
-      },
-      (compilation, callback) => {
-        this.done = callback
-        this.workers.forEach((worker) => {
-          try {
-            process.kill(worker.process.pid, 'SIGTERM')
-          } catch (e) {
-            // eslint-disable-next-line
-            console.warn(`Unable to kill process #${worker.process.pid}`)
-          }
-        })
+	apply(compiler) {
+		compiler.hooks.afterEmit.tap(
+			{
+				name: 'reload-server',
+			},
+			(compilation, callback) => {
+				this.done = callback
+				this.workers.forEach((worker) => {
+					try {
+						process.kill(worker.process.pid, 'SIGTERM')
+					} catch (e) {
+						// eslint-disable-next-line
+						console.warn(`Unable to kill process #${worker.process.pid}`)
+					}
+				})
 
-        this.workers = []
+				this.workers = []
 
-        cluster.fork()
-      }
-    )
-  }
+				cluster.fork()
+			}
+		)
+	}
 }
 
 module.exports = ReloadServerPlugin
