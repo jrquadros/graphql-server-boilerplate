3048ac7bb2d13e7b2dbfbf1e31b9194f3a873235
jrquadros <jrmquadros@gmail.com>
2021-08-15 20:45:11 -0300

    feat(server): add express playground

--- src/server/App.ts
+++ src/server/App.ts
@@ -1,12 +1,15 @@
 import * as express from 'express'
 
+import ExpressPlayground from 'graphql-playground-middleware-express'
 import { graphqlHTTP } from 'express-graphql'
 import { schema } from '../graphql/schema'
 
 const app = express()
 
-const graphql = graphqlHTTP({ schema, graphiql: true })
+const graphql = graphqlHTTP({ schema, graphiql: false })
 
 app.use('/graphql', graphql)
+
+app.get('/playground', ExpressPlayground({ endpoint: '/graphql' }))
 
 export const App = app
